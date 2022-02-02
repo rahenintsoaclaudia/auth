77b3681643ae5bd8e8a7714574db6b0c878bd428
RAHENINTSOA Marie Claudia <marie-claudia.r@itdm-group@gmail.com>
2022-02-02 14:21:50 +0100

    debugage serveur.js

--- serveur.js
+++ serveur.js
@@ -4,7 +4,7 @@ const path = require("path")
 const app = express()
 
 
-app.use(express.static(__dirname + 'dist/auth-google'));
+app.use(express.static(__dirname + '/dist/auth-google'));
 
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/dist/auth-google/index.html'));
